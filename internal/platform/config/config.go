06dea62b1abf96d41a7f249fcf71a8a5e04f5e60
Vitalii Timoshenko <iamroockie@proton.me>
2026-08-21 18:47:52 +0600

    refactor: add validate and Addr method to HTTPConfig

--- internal/platform/config/config.go
+++ internal/platform/config/config.go
@@ -13,17 +13,8 @@ type Config struct {
 	Env                     Environment   `env:"APP_ENV,notEmpty"`
 	LogLevel                slog.Level    `env:"LOG_LEVEL" envDefault:"info"`
 	PostgresDSN             string        `env:"POSTGRES_DSN,notEmpty"`
-	GracefulShutdownTimeout time.Duration `env:"GRACEFUL_SHUTDOWN_TIMEOUT"  envDefault:"30s"`
+	GracefulShutdownTimeout time.Duration `env:"GRACEFUL_SHUTDOWN_TIMEOUT" envDefault:"30s"`
 	HTTP                    HTTPConfig    `envPrefix:"HTTP_"`
-}
-
-type HTTPConfig struct {
-	Host              string        `env:"HOST" envDefault:"0.0.0.0"`
-	Port              uint16        `env:"PORT" envDefault:"8080"`
-	ReadHeaderTimeout time.Duration `env:"READ_HEADER_TIMEOUT" envDefault:"5s"`
-	ReadTimeout       time.Duration `env:"READ_TIMEOUT" envDefault:"10s"`
-	WriteTimeout      time.Duration `env:"WRITE_TIMEOUT" envDefault:"10s"`
-	IdleTimeout       time.Duration `env:"IDLE_TIMEOUT" envDefault:"60s"`
 }
 
 func Load() (Config, error) {
@@ -44,24 +35,8 @@ func (c Config) validate() error {
 		return err
 	}
 
-	if c.HTTP.Port == 0 {
-		return errors.New("HTTP_PORT can not be 0")
-	}
-
-	if c.HTTP.ReadTimeout <= 0 {
-		return errors.New("HTTP_READ_TIMEOUT must be greater than 0")
-	}
-
-	if c.HTTP.WriteTimeout <= 0 {
-		return errors.New("HTTP_WRITE_TIMEOUT must be greater than 0")
-	}
-
-	if c.HTTP.ReadHeaderTimeout <= 0 {
-		return errors.New("HTTP_READ_HEADER_TIMEOUT must be greater than 0")
-	}
-
-	if c.HTTP.IdleTimeout <= 0 {
-		return errors.New("HTTP_IDLE_TIMEOUT must be greater than 0")
+	if err := c.HTTP.validate(); err != nil {
+		return err
 	}
 
 	if c.GracefulShutdownTimeout <= 0 {
---
+++ internal/platform/config/http.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"errors"
+	"net"
+	"strconv"
+	"time"
+)
+
+type HTTPConfig struct {
+	Host              string        `env:"HOST" envDefault:"0.0.0.0"`
+	Port              uint16        `env:"PORT" envDefault:"8080"`
+	ReadHeaderTimeout time.Duration `env:"READ_HEADER_TIMEOUT" envDefault:"5s"`
+	ReadTimeout       time.Duration `env:"READ_TIMEOUT" envDefault:"10s"`
+	WriteTimeout      time.Duration `env:"WRITE_TIMEOUT" envDefault:"10s"`
+	IdleTimeout       time.Duration `env:"IDLE_TIMEOUT" envDefault:"60s"`
+}
+
+func (hc HTTPConfig) Addr() string {
+	portStr := strconv.FormatUint(uint64(hc.Port), 10)
+
+	return net.JoinHostPort(hc.Host, portStr)
+}
+
+func (hc HTTPConfig) validate() error {
+	if hc.Port == 0 {
+		return errors.New("HTTP_PORT can not be 0")
+	}
+
+	if hc.ReadTimeout <= 0 {
+		return errors.New("HTTP_READ_TIMEOUT must be greater than 0")
+	}
+
+	if hc.WriteTimeout <= 0 {
+		return errors.New("HTTP_WRITE_TIMEOUT must be greater than 0")
+	}
+
+	if hc.ReadHeaderTimeout <= 0 {
+		return errors.New("HTTP_READ_HEADER_TIMEOUT must be greater than 0")
+	}
+
+	if hc.IdleTimeout <= 0 {
+		return errors.New("HTTP_IDLE_TIMEOUT must be greater than 0")
+	}
+
+	return nil
+}
