d8cf9cf23c945c7f59cb644b71675b2985eda9b5
Vitalii Timoshenko <iamroockie@proton.me>
2026-08-21 18:49:19 +0600

    refactor: move run server from main to app/api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
 	"net"
-	"net/http"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
 
+	"github.com/iamroockie/parterre/internal/app/api"
 	"github.com/iamroockie/parterre/internal/platform/config"
-	"github.com/iamroockie/parterre/internal/platform/httpx"
 	"github.com/iamroockie/parterre/internal/platform/logger"
 )
 
@@ -29,47 +26,19 @@ func run() error {
 		return err
 	}
 
-	logFormat := logFormatFromEnv(cfg.Env)
-	log := logger.New(os.Stdout, logFormat, cfg.LogLevel)
+	log := logger.New(os.Stdout, logFormatFromEnv(cfg.Env), cfg.LogLevel)
 	slog.SetDefault(log)
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
-	addr := net.JoinHostPort(cfg.HTTP.Host, strconv.FormatUint(uint64(cfg.HTTP.Port), 10))
-	srv := http.Server{
-		Addr:              addr,
-		ReadHeaderTimeout: cfg.HTTP.ReadHeaderTimeout,
-		ReadTimeout:       cfg.HTTP.ReadTimeout,
-		WriteTimeout:      cfg.HTTP.WriteTimeout,
-		IdleTimeout:       cfg.HTTP.IdleTimeout,
-		Handler:           httpx.NewRouter(log),
+	var lc net.ListenConfig
+	ln, err := lc.Listen(ctx, "tcp", cfg.HTTP.Addr())
+	if err != nil {
+		return err
 	}
 
-	srvError := make(chan error, 1)
-	go func() {
-		log.Info("server running", "addr", srv.Addr)
-		srvError <- srv.ListenAndServe()
-	}()
-
-	select {
-	case err := <-srvError:
-		return fmt.Errorf("server error: %w", err)
-	case <-ctx.Done():
-	}
-
-	log.Info("shutdown started")
-
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), cfg.GracefulShutdownTimeout)
-	defer cancel()
-
-	if err := srv.Shutdown(shutdownCtx); err != nil {
-		return fmt.Errorf("shutdown error: %w", err)
-	}
-
-	log.Info("shutdown finished")
-
-	return nil
+	return api.Run(ctx, cfg, log, ln)
 }
 
 func logFormatFromEnv(env config.Environment) logger.Format {
