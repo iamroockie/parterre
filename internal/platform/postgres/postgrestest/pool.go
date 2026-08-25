a85832732f4d4acb7c459318d7b7606107fb8f26
Vitalii Timoshenko <iamroockie@proton.me>
2026-08-25 10:24:29 +0600

    test: apply migrations to test database

--- internal/platform/postgres/postgrestest/pool.go
+++ internal/platform/postgres/postgrestest/pool.go
@@ -1,13 +1,21 @@
 package postgrestest
 
 import (
+	"database/sql"
+	"io/fs"
 	"testing"
 
 	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/pressly/goose/v3"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	// driver pgx for database/sql, required by goose
+	_ "github.com/jackc/pgx/v5/stdlib"
+
+	"github.com/iamroockie/parterre/internal/platform/postgres/migrations"
 )
 
 func NewTestPool(t *testing.T) *pgxpool.Pool {
@@ -44,5 +52,24 @@ func NewTestPool(t *testing.T) *pgxpool.Pool {
 
 	require.NoError(t, pool.Ping(ctx))
 
+	applyMigrations(t, dsn)
+
 	return pool
+}
+
+func applyMigrations(t *testing.T, dsn string) {
+	t.Helper()
+
+	db, err := sql.Open("pgx", dsn)
+	require.NoError(t, err)
+	defer func() { _ = db.Close() }()
+
+	sub, err := fs.Sub(migrations.FS, "sql")
+	require.NoError(t, err)
+
+	p, err := goose.NewProvider(goose.DialectPostgres, db, sub)
+	require.NoError(t, err)
+
+	_, err = p.Up(t.Context())
+	require.NoError(t, err)
 }
